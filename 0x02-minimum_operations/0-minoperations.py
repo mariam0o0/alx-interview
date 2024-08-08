ac90edee5d68187a43559f8b9056df66cfa6e940
Mariam <mariam.yasser772000@gmail.com>
2024-08-08 23:57:03 +0000

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" Minimum Operations"""
+""" Script that computes a minimum operations"""
 
 
-def minOperations(n: int) -> int:
-    """ Calculates the fewest number of operations to result in exactly n H characters """
-    op = 0
-    body = 'H'
-    next = 'H'
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+def minOperations(n):
+    """compute the minimum number of operations """
+    if n < 2:
         return 0
-    return op
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+    return sum(factor_list)
