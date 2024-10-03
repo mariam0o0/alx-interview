f644dbe20540177ec55001954e836ed3acbf6b8b
Mariam <mariam.yasser772000@gmail.com>
2024-10-03 19:34:22 +0000

    done

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module for Prime Game"""
+
+
+def isWinner(x, nums):
+    """Determines the winner of a set of prime number removal games."""
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+    ben = 0
+    maria = 0
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_multiples(ls, x):
+    """Removes multiples of a prime number from an array of possible prime"""
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
